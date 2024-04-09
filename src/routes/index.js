43e03372ce18ad046f1494675c38b3ea3d8f8682
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2024-04-09 05:58:31 -0300

    fix: Update server routes to include dishes routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ const {Router} = require("express");
 
 const userRoutes = require("./indexers/user.routes");
 const categoriesRoutes = require("./indexers/categories.routes");
+const dishesRoutes = require("./indexers/dishes.routes");
 const sessionsRoutes = require("./indexers/sessions.routes");
 
 const routes = Router();
 
 routes.use("/user", userRoutes);
 routes.use("/categories", categoriesRoutes);
+routes.use("/dishes", dishesRoutes);
 routes.use("/sessions", sessionsRoutes);
 
 module.exports = routes;
