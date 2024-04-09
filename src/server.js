e252d65f42e8e3530e04927250414b7f978fc2bf
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2024-04-09 01:15:25 -0300

    feat(middlewares): Aplying AppError middleware on server main file

--- src/server.js
+++ src/server.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const server = express();
 const port = process.env.SERVER_PORT;
+const AppError = require("./middlewares/AppError");
 
+server.use((error, request, response, next) => {
+  console.log(error);
+  if(error instanceof AppError){
+    return response.status(error.statusCode).json({
+      message: error.message,
+    });
+  };
+  response.status(500).json({
+    message: "Erro interno do servidor."
+  })
+});
 server.listen(port, ()=>{
   console.log(`Server is running on port ${port}`);
 });
