06841204802a7a5b1ba4e7ab478596c053b4dd56
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2024-04-09 05:37:46 -0300

    Revert "feat: Creating users routes and controller"
    This reverts commit 16b666ebe19649b7800c3b71e52f6b798328ec3a.

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require("express");
-
-const userRoutes = require("./indexers/user.routes");
-
-const routes = Router();
-
-routes.use("/user", userRoutes);
-
-module.exports = routes;
